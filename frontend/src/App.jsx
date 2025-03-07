bbc3346a08fcc0cd664e220a276dc391c225a0cc
Fahim047 <fahimulislam58@gmail.com>
2025-03-07 09:02:20 +0600

    ✨ feat: add shadcn ui component library

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import { Button } from './components/ui/button';
 
 const App = () => {
 	return (
 		<div>
 			<h1 className="text-green-600">Bismillah!</h1>
+			<Button>Hello</Button>
 		</div>
 	);
 };
