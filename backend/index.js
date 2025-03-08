4b86fd427d58ecdcbf2615e87c69ed95dd68794f
Fahim047 <fahimulislam58@gmail.com>
2025-03-08 10:28:46 +0600

    ✨ feat: add postgres connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
+import pool from './src/config/db.js';
 import { PORT } from './src/config/env.js';
 
 const app = express();
@@ -10,8 +11,10 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+app.get('/', async (req, res) => {
+	const result = await pool.query('SELECT current_database()');
+	const dbName = result.rows[0].current_database;
+	res.send(`Connected to ${dbName}`);
 });
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -2,4 +2,12 @@ import { config } from 'dotenv';
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
-export const { PORT, NODE_ENV } = process.env;
+export const {
+	PORT,
+	NODE_ENV,
+	DB_HOST,
+	DB_PORT,
+	DB_NAME,
+	DB_USER,
+	DB_PASSWORD,
+} = process.env;
